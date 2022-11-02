5c854207a77aa00a1cf526e66119a9958a3e670b
FeczIne <mashirotj@gmail.com>
2022-11-02 14:17:21 +0800

    fix bug

--- src/main/java/ski/mashiro/data/DropItemData.java
+++ src/main/java/ski/mashiro/data/DropItemData.java
@@ -17,7 +17,6 @@ import java.util.concurrent.*;
 @SuppressWarnings("AlibabaThreadShouldSetName")
 public class DropItemData {
     private static final ScheduledExecutorService SCHEDULED_POOL = new ScheduledThreadPoolExecutor(1);
-    private static final ThreadPoolExecutor POOL = new ThreadPoolExecutor(1, 1, 30, TimeUnit.SECONDS, new ArrayBlockingQueue<>(1));
     public static void clearDropItem(Plugin plugin) {
         if (!ConfigFiles.config.isEnableCleanDropItem()) {
             return;
@@ -29,6 +28,7 @@ public class DropItemData {
                 Bukkit.broadcastMessage(Utils.transferTimePlaceHolder(MessageFiles.message.getClearTime(), String.valueOf(15)));
                 Thread.sleep(10000);
                 Bukkit.broadcastMessage(Utils.transferTimePlaceHolder(MessageFiles.message.getClearTime(), String.valueOf(5)));
+                Thread.sleep(5000);
                 Bukkit.getScheduler().runTask(plugin, () -> {
                     int sum = 0;
                     for (World world : Bukkit.getWorlds()) {
